36d5af58696182f494178956532fafc5a8b7c39f
ksauzz <ksauzzmsg@gmail.com>
2012-07-11 17:50:19 +0900

    bugfix: handling nil

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -249,7 +249,7 @@ class ImporterController < ApplicationController
         end
         assigned_to = row[attrs_map["assigned_to"]] != nil ? user_for_login!(row[attrs_map["assigned_to"]]) : nil
         fixed_version_name = row[attrs_map["fixed_version"]]
-        fixed_version_id = fixed_version_name ? version_id_for_name!(project,fixed_version_name,add_versions) : nil
+        fixed_version_id = !fixed_version_name.empty? ? version_id_for_name!(project,fixed_version_name,add_versions) : nil
         watchers = row[attrs_map["watchers"]]
         # new issue or find exists one
         issue = Issue.new
